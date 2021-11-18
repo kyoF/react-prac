7effd1fc3bdd397977ffc7d6a8b4529ee62400ce
ekusoyk <fujiki.kyosuke@gmail.com>
2021-11-18 23:25:48 +0900

    サイトの改修までは完了

--- react-ts-app-prac/src/index.tsx
+++ react-ts-app-prac/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Repeat } from "typescript-tuple";
+import { useState } from "react";
 import "./index.css";
 
 type SquareState = "O" | "X" | null;
@@ -9,17 +10,17 @@ type SquareProps = {
   onClick: () => void;
 };
 
-type BoardState = Repeat<SquareState, 9>;
-type BoardProps = {
-  squares: BoardState;
-  onClick: (i: number) => void;
-};
-
 const Square = (props: SquareProps) => (
   <button className="square" onClick={props.onClick}>
     {props.value}
   </button>
 );
+
+type BoardState = Repeat<SquareState, 9>;
+type BoardProps = {
+  squares: BoardState;
+  onClick: (i: number) => void;
+};
 
 const Board = (props: BoardProps) => {
   const renderSquare = (i: number) => (
@@ -46,21 +47,58 @@ const Board = (props: BoardProps) => {
   );
 };
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+type Step = {
+  readonly squares: BoardState;
+  readonly xIsNext: boolean;
+};
+
+type GameState = {
+  readonly history: Step[];
+  readonly stepNumber: number;
+};
+
+const Game = () => {
+  const [state, setState] = useState<GameState>({
+    history: [
+      {
+        squares: [null, null, null, null, null, null, null, null, null],
+        xIsNext: true,
+      },
+    ],
+    stepNumber: 0,
+  });
+
+  const current = state.history[state.stepNumber];
+
+  const handleClick = (i: number) => {
+    setState(({ history, stepNumber }) => {
+      const newHistory = history.slice(0, stepNumber + 1).concat(next);
+      return {
+        history: newHistory,
+        stepNumber: newHistory.length - 1,
+      };
+    });
+  };
+
+  const jumpTo = (move: number) => {
+    setState((prev) => ({
+      ...prev,
+      stepNumber: move,
+    }));
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={handleClick} />
       </div>
-    );
-  }
-}
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
 
 // ========================================
 
