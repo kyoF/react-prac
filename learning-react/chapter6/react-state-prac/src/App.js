5d62a837a4a12d4e42fd2e6b79d278411efe937e
kyoF <fujiki.kyosuke@gmail.com>
2022-03-04 23:00:55 +0900

    fasterのコンポーネント作成

--- learning-react/chapter6/react-state-prac/src/App.js
+++ learning-react/chapter6/react-state-prac/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FaSter } from "react-icons/fa";
 
 function App() {
   return (
---
+++ learning-react/chapter6/react-state-prac/src/components/Faster.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaSter } from "react-icons/fa";
+
+export default function StarRating() {
+  return [
+    <FaSter color="red" />,
+    <FaSter color="red" />,
+    <FaSter color="red" />,
+    <FaSter color="grey" />,
+    <FaSter color="grey" />,
+  ];
+}
